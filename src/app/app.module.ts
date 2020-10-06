7a115c08fdc83a8ebc65354f898362c3690c30ed
pratik-03 <57658783+pratik-03@users.noreply.github.com>
2020-10-06 17:00:08 +0530

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    ProductListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
+  popModal = false;
+  products;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  product:Array<any>=[
+    { ProductId:1,ProductName:"Nike Running shies" ,image:'./assets/nike.jpg', Category:"Shoe", price:1200 },
+    { ProductId:1,ProductName:"Jacket" ,image:'./assets/jacket.jpg', Category:"jacket", price:1000 },
+    { ProductId:1,ProductName:"Shirt" ,image:'https://images-eu.ssl-images-amazon.com/images/I/41xCWDx-OyL._SX342_QL70_ML2_.jpg', Category:"Shirts", price:800 },
+  ];
+
+
+  onGetData(productid){
+   var prod  =  this.product?.filter(value=>{
+      value.ProductId == productid
+    });
+    this.products= prod;
+   this.popModal = true;
+  }
+
+  onHandlePopup(){
+    this.popModal =false;
   }
 
 }
